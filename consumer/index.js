d4b2aa5b28dc416bf586be06ac1f24d89dc32023
Oleg Ivanov <olivanov87@gmail.com>
2021-04-03 18:06:02 +0300

    Moved the tracing configuration to a separate file

--- consumer/index.js
+++ consumer/index.js
@@ -1,39 +1,4 @@
-const opentelemetry = require('@opentelemetry/api');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing');
-const { NodeTracerProvider } = require('@opentelemetry/node');
-const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
-const { KafkaJsInstrumentation } = require('opentelemetry-instrumentation-kafkajs');
-
-const collectorOptions = {
-  serviceName: 'consumer-service'
-};
-
-// Create and configure NodeTracerProvider
-const traceProvider = new NodeTracerProvider({
-  // be sure to disable old plugin
-  plugins: {
-    kafkajs: { enabled: false, path: 'opentelemetry-plugin-kafkajs' }
-  }
-});
-
-const exporter = new CollectorTraceExporter(collectorOptions);
-traceProvider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-traceProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-traceProvider.register()
-
-registerInstrumentations({
-  traceProvider,
-  instrumentations: [
-    new KafkaJsInstrumentation({
-      // see under for available configuration
-    })
-  ]
-});
-
-const tracer = opentelemetry.trace.getTracer('consumer-service');
-
+const { tracer } = require ('./trace')
 const { Kafka, logLevel } = require('kafkajs')
 
 const kafka = new Kafka({
---
+++ producer/trace.js
@@ -0,0 +1,35 @@
+const opentelemetry = require('@opentelemetry/api');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { NodeTracerProvider } = require('@opentelemetry/node');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+const { KafkaJsInstrumentation } = require('opentelemetry-instrumentation-kafkajs');
+
+const collectorOptions = {
+  serviceName: 'producer-service'
+};
+
+// Create and configure NodeTracerProvider
+const traceProvider = new NodeTracerProvider({
+  // be sure to disable old plugin
+  plugins: {
+    kafkajs: { enabled: false, path: 'opentelemetry-plugin-kafkajs' }
+  }
+});
+
+const exporter = new CollectorTraceExporter(collectorOptions);
+traceProvider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+traceProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+traceProvider.register()
+
+registerInstrumentations({
+  traceProvider,
+  instrumentations: [
+    new KafkaJsInstrumentation({
+      // see under for available configuration
+    })
+  ]
+});
+
+const tracer = opentelemetry.trace.getTracer('producer-service');
